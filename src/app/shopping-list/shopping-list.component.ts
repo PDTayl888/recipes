4a074c897e664200a0485b889e9c0b8ad4d69850
Koryn Noah-Patrick <koryn78@koryns-mbp.fios-router.home>
2019-06-08 23:35:37 -0400

    message

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -21,7 +21,6 @@ export class ShoppingListComponent implements OnInit {
     onIngredientAdded(ingredient: Ingredient) {
       console.log('onIngredientAdded called');
       console.log(ingredient);
-      this.ingredients.push(ingredient);
-      
+      this.ingredients.push(ingredient); 
     }
 }
